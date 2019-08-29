475b5f896dbd67009a609439d08880892f56a0a7
Aluno <aluno@etec.sp.gov.br>
2019-08-29 15:46:20 -0300

    demonstracao

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+         <link href="css/estilos.css" type="text/css" rel="stylesheet" />
+    </head>
+    <body>
+        <div id="global">
+             <h1>GESCOLAR</h1>
+             <nav>
+                 <ul>
+                     <li><a href="cadastro_alunos.php">Cadastrar Aluno</a></li>
+                     <li><a href="lista_alunos.php">Lista de Alunos</a><li>
+                 </ul>
+             </nav>
+        </div>
+    </body>
+</html>
