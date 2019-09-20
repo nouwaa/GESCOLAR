6118f3d40c23206c3d1e25f8c5ae688a390ea49d
Aluno <aluno@etec.sp.gov.br>
2019-09-20 10:14:50 -0300

    mudanca

---
+++ includes/cabecalho.php
@@ -0,0 +1,9 @@
+<header>
+    <ul id="menu-navegacao">
+        <li> <a href="cadastro_alunos.php">Novo Aluno</a> </li>
+        <li> <a href="lista_alunos.php">Lista de Alunos</a> </li>
+
+        <li> <a href="cadastro_curso.php">Novo Curso</a> </li>
+        <li> <a href="lista_cursos.php">Lista de Cursos</a> </li>
+    </ul>
+</header>  
